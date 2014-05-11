b69534a15a5b6d4a2306bf66c5d8d60488030998
Radoslav Mladenov <rvmladenov@gmail.com>
2014-05-11 16:43:10 +0300

    Modified the files and included PHP and MVC to the site.

---
+++ mk_home/views/index/index.php
@@ -0,0 +1,15 @@
+<section id="main_content">
+    <div id="central_content">
+        <div class="row top right"></div>
+        <div class="row"></div>
+        <div class="row top left"></div>
+        <div class="central">
+            <div id="logo">
+                <a href="javascript:"></a>
+            </div>
+        </div>
+        <div class="row bottom right" ></div>
+        <div class="row"></div>
+        <div class="row bottom left"></div>
+    </div>
+</section>

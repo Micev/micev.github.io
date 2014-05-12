d76977d21b908653de377e934019d790b4d335d1
Georgi <Micev>
2014-05-12 12:03:14 +0300

    add new nav tag and add new section

--- mk_home/views/index/index.php
+++ mk_home/views/index/index.php
@@ -1,15 +1,32 @@
 <section id="main_content">
-    <div id="central_content">
-        <div class="row top right"></div>
-        <div class="row"></div>
-        <div class="row top left"></div>
-        <div class="central">
-            <div id="logo">
-                <a href="javascript:"></a>
+    <nav>
+        <div id="central_content">
+            <div class="row top right">
+                <section class="section_box top_left">
+                    
+                </section>
+            </div>
+            <div class="row"></div>
+            <div class="row top left">
+              <section class="section_box top_right">
+
+              </section>
+            </div>
+            <div class="central">
+                <div id="logo">
+                    <a href="javascript:"></a>
+                </div>
+            </div>
+            <div class="row bottom right" >
+              <section class="section_box down_left">
+              </section>
+            </div>
+            <div class="row"></div>
+            <div class="row bottom left">
+              <section class="section_box down_right">
+
+              </section>
             </div>
         </div>
-        <div class="row bottom right" ></div>
-        <div class="row"></div>
-        <div class="row bottom left"></div>
-    </div>
+    </nav>
 </section>
